69676ca20f34bcfcaca78121033fc9730f014db8
Nusratxoja21 <ganixojayev@internet.ru>
2022-07-26 12:25:29 +0500

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Nusrat Ganiyev on 7/26/22.
+//  Copyright © 2022 Nusrat Ganiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
